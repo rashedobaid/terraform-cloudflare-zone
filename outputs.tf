a679a32467096cfc62227cf6cfdb408e0205dd62
Rashed Obaid <mail@rashedobaid.com>
2025-06-03 19:13:40 +0800

    Fix record key to id output

--- outputs.tf
+++ outputs.tf
@@ -5,7 +5,7 @@ output "id" {
 
 output "record_key_to_id" {
   description = "Map of record keys (name-type-content) to record IDs."
-  value       = { for k, record in cloudflare_dns_record.default : k => record.id... if local.records_enabled }
+  value       = { for k, record in cloudflare_dns_record.default : k => record.id }
 }
 
 output "type" {
